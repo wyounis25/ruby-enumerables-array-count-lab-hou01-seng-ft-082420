647a7bce13ae0d955265181e0b54af960c8e40fb
walid younis <walid.younis.90@gmail.com>
2020-08-19 23:00:07 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,10 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+ 
+array.count do |num|
+  num == 1
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+ array.count do |empty|
+   empty == ""
 end
